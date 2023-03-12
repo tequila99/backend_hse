ee86e74f76f112d21dd6aa1e3fc4ac67294d3845
Ivan Milovanov <tequila00@yandex.ru>
2023-03-12 17:42:39 +0300

    [feat]: Добавление приоритета в задачу

--- core/models.py
+++ core/models.py
@@ -5,3 +5,4 @@ class TaskModel(models.Model):
     name = models.CharField(max_length=250, verbose_name="Имя")
     description = models.CharField(max_length=2000, verbose_name="Описание")
     is_done = models.BooleanField(default=False)
+    priority = models.CharField(default="medium", max_length=6, verbose_name="Приоритет")
